195d182e2532fd514bd1db2f7b7370c81172d793
mrchocoswirl <ltbaldelomar9@gmail.com>
2015-01-17 06:20:56 +0800

    Add qtip plugin and fix layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 $(document).foundation();
 
 $(document).ready(function(){
-    $('a.degdeg').each(function() {
+    $('a.degdeg').each(function(){
         var id = $(this).attr('id');
         $('a#'+id).zclip({
             path: 'js/zclip/ZeroClipboard.swf',
-            copy: function() {return $('a#'+id).text();}
+            copy: function(){return $('a#'+id).text();}
         });
     });
 });
