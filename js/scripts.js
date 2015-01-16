7eebd3058e5647e038093601735355a1918f01d9
mrchocoswirl <ltbaldelomar9@gmail.com>
2015-01-17 07:36:17 +0800

    Replaced JS prompt with qtip

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,23 @@ $(document).ready(function(){
         var id = $(this).attr('id');
         $('a#'+id).zclip({
             path: 'js/zclip/ZeroClipboard.swf',
-            copy: function(){return $('a#'+id).text();}
+            copy: function(){return $('a#'+id).text();},
+            afterCopy: function(){}
         });
+    });
+    $('a.degdeg').qtip({
+        content: 'Copied!',
+        show: {
+            event: 'click',
+            solo: true,
+            effect: function() {
+                $(this).fadeTo(200, 1);
+            }
+        },
+        hide: {
+            effect: function() {
+                $(this).animate({ opacity: 0 }, { duration: 1000 });
+            }
+        }
     });
 });
